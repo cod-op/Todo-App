cfccdd8636d8cc8dcf85bfd12d66c27b1f57d0a5
cod-op <shlokpatel094@gmail.com>
2026-03-27 17:31:19 +0530

    frontend

--- frontend/src/Service/AuthService.jsx
+++ frontend/src/Service/AuthService.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const registerUser = (data) => {
-  return axios.post("https://todo-app-backend-8gmt.onrender.com/api/v1/user/register", data);
+  return axios.post("http://localhost:7000/api/v1/user/register", data);
 };
 
 const loginUSer = (data) => {
-  return axios.post("https://todo-app-backend-8gmt.onrender.com/api/v1/user/login", data);
+  return axios.post("http://localhost:7000/api/v1/user/login", data);
 };
 
 const AuthServices = { registerUser, loginUSer };
